5aca88eeba38425c4424221ff90be9b1ba3d5f82
Serg <bsi_acs@mail.ru>
2020-11-30 12:40:26 +0300

    Испралена логика получения данных о сотрудниках

---
+++ WebStore/Data/EmployeesInfoProvider.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Models;
+
+namespace WebStore.Data
+{
+    public static class EmployeesInfoProvider
+    {
+        private static IEnumerable<Employee> employees = new List<Employee>() {
+            new Employee() { Id = 1, Name = "Денис", Surename = "Огурцов", Patronymic = "Сергеевич", Gender = "Мужской", Age = 38, Profession = "Менеджер по заказам"},
+            new Employee() { Id = 2, Name = "Ольга", Surename = "Самохвалова", Patronymic = "Викторовна", Gender = "Женский", Age = 32, Profession = "Бухгалтер"},
+            new Employee() { Id = 3, Name = "Виктор", Surename = "Быков", Patronymic = "Николаевич", Gender = "Мужской", Age = 55, Profession = "Главный бухгалтер"},
+            new Employee() { Id = 4, Name = "Олег", Surename = "Калинин", Patronymic = "Викторович", Gender = "Мужской", Age = 38, Profession = "Контент-менеджер"},
+            new Employee() { Id = 5, Name = "Юрий", Surename = "Корюнин", Patronymic = "Анатольевич", Gender = "Мужской", Age = 46, Profession = "Менеджер по заказам"},
+            new Employee() { Id = 6, Name = "Анна", Surename = "Огурцова", Patronymic = "Александровна", Gender = "Женский", Age = 35, Profession = "Кладовщик"},
+        };
+
+        public static IEnumerable<Employee> Employees {
+            get => employees;
+        }
+    }
+}
