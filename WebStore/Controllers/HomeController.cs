3d0c7e44fc35cba0f94889f9d08363919d9d16ec
Serg <bsi_acs@mail.ru>
2020-12-01 14:50:07 +0300

    Функционал для работы со списком сотрудников перенесён в отдельный контроллер

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -29,49 +29,6 @@ namespace WebStore.Controllers
         public IActionResult ProductDetails() => View();
 
         public IActionResult Shop() => View();
-
-
-        public IActionResult EmployeesList()
-        {
-            return View(EmployeesInfoProvider.Employees);
-        }
-
-        public IActionResult Details(int id)
-        {
-            Employee employee = EmployeesInfoProvider.Employees.FirstOrDefault(emp => emp.Id == id);
-
-            return View(employee);
-        }
-
-        public IActionResult Delete(int id)
-        {
-            EmployeesInfoProvider.Employees = EmployeesInfoProvider.Employees.Where( emp => emp.Id != id);
-
-            return View("EmployeesList", EmployeesInfoProvider.Employees);
-        }
-
-        [HttpGet]
-        public IActionResult Edit(int id)
-        {
-            Employee employee = EmployeesInfoProvider.Employees.FirstOrDefault(emp => emp.Id == id);
-
-            return View(employee);
-        }
-
-        [HttpPost]
-        public IActionResult Edit(Employee employee)
-        {
-            Employee emp = EmployeesInfoProvider.Employees.FirstOrDefault( item => item.Id == employee.Id);
-
-            if (emp != null) {
-                emp.Name = employee.Name;
-                emp.Surename = employee.Surename;
-                emp.Patronymic = employee.Patronymic;
-                emp.Age = employee.Age;
-                emp.Profession = employee.Profession;
-            }
-
-            return View("EmployeesList", EmployeesInfoProvider.Employees);
-        }
+        
     }
 }
