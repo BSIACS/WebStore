fda85433d2ed4e0646096f6a7e7a93c98b780900
Serg <bsi_acs@mail.ru>
2021-03-18 16:19:32 +0300

    Добавлена область Admin

---
+++ WebStore/Areas/Admin/Controllers/CatalogueController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Domain.Entities;
+using WebStore.Infrastructure.Interfaces;
+using WebStore.Infrastructure.Services.InSqlDataBase;
+using WebStore.Infrastructure.Mapping;
+using WebStore.ViewModels;
+
+namespace WebStore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CatalogueController : Controller
+    {
+        private IProductData _productData;
+
+        public CatalogueController(IProductData productData)
+        {
+            _productData = productData;
+        }
+
+        public IActionResult Index()
+        {
+            var products = _productData.GetProducts();
+
+            return View(products);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id) {
+            var product = _productData.GetProductById(id);
+
+            if (product == null)
+                return NotFound();
+
+            return View(product.ToViewModel());
+        }
+
+        [HttpPost]
+        public IActionResult Edit(ProductViewModel model) {
+
+            if (!ModelState.IsValid)
+                return View(model);
+
+            //Дописать логику
+
+            return RedirectToAction("Index");
+        }
+    }
+}
--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -98,6 +98,10 @@ namespace WebStore
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapControllerRoute(
+                name: "areas",
+                pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
                 endpoints.MapControllerRoute(                                           // Определение маршрутов
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}");
